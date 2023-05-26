d026925d71e545fa37096e91adf36cedc298a9d1
saarahabdel <saarahabdel15@gmail.com>
2023-05-27 01:09:57 +0200

    Completed DWA Lesson1 & Added Challenge 1 files.

---
+++ DWA5/DWA5.3_Challenge1/script.js
@@ -0,0 +1,9 @@
+const form = document.querySelector("[data-form]");
+const result = document.querySelector("[data-result]");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const entries = new FormData(event.target);
+  const { dividend, divider } = Object.fromEntries(entries);
+  result.innerText = dividend / divider;
+});
