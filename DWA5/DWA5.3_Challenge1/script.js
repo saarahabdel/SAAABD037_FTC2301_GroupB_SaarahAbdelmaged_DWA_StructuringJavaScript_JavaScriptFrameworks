328e1021c4c627fd4fe7d94b29d14242fbbfc9ed
saarahabdel <saarahabdel15@gmail.com>
2023-05-28 15:23:52 +0200

    Completed DWA5.3 Challenge 1.

--- DWA5/DWA5.3_Challenge1/script.js
+++ DWA5/DWA5.3_Challenge1/script.js
@@ -5,5 +5,22 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
-  result.innerText = dividend / divider;
+
+  if (!dividend || !divider) {
+    result.innerText = 'Division not performed. Both values are required in inputs. Try again.';
+  } else if (dividend < 0 || divider < 0) {
+    const error = new Error('Division not performed. Invalid number provided. Try again.');
+    console.log(error);
+    result.innerText = error;
+  } else if (isNaN(dividend) || isNaN(divider)) {
+    const criticalError = new Error('Something critical went wrong. Please reload page.');
+    const bodyHTML = document.querySelector('body')
+    console.log(criticalError);
+    bodyHTML.innerText = criticalError;
+  } else {
+    result.innerText = Math.floor(dividend / divider);
+  }
 });
+
+
+
