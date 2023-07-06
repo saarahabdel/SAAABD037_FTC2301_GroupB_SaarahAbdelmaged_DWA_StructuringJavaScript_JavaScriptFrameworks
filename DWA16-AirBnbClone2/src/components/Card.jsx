4905c750d6fc9db5345b4ca4eb16d305c33b254a
SaarahAbdelmaged <saarahabdel15@gmail.com>
2023-07-06 13:44:21 +0200

    Completed DWA16.

--- DWA16-AirBnbClone2/src/components/Card.jsx
+++ DWA16-AirBnbClone2/src/components/Card.jsx
@@ -19,7 +19,7 @@ export default function Card(props) {
                 className="card--image" 
             />
             <div className="card--stats">
-                <img src="../images/star.png" className="card--star" />
+                <img src="/star.png" className="card--star" />
                 <span>{props.stats.rating}</span>
                 <span className="gray">({props.stats.reviewCount}) • </span>
                 <span className="gray">{props.location}</span>
