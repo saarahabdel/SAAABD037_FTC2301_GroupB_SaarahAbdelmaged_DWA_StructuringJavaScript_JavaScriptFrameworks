1b9a28b80c77427b8cd61c0ed6514d69c05a08f8
SaarahAbdelmaged <saarahabdel15@gmail.com>
2023-07-08 20:35:27 +0200

    Started on DWA17-MemeGenerator.

---
+++ DWA17-MemeGenerator/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import Header from "../src/components/Header"
+import Meme from "../src/components/Meme"
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <Meme />
+    </div>
+  )
+}
